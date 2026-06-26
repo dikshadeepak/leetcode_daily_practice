05e58ad1ca7185c759982f81d6fd6950b0bc0448
Diksha Deepak <dikshadeepak0195@gmail.com>
2026-06-26 22:36:44 +0530

    Time: 4 ms (2.38%), Space: 29 MB (8.84%) - LeetHub

--- 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.cpp
+++ 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     bool hasTrailingZeros(vector<int>& nums) {
-        int even = 0;
+        int n = nums.size();
 
-        for (int x : nums) {
-            if ((x & 1) == 0)
-                even++;
+        int i = 0;
+        int j = 1;
+
+        while (i < n - 1) {
+            if ((nums[i] | nums[j]) % 2 == 0)
+                return true;
+
+            j++;
+
+            if (j == n) {
+                i++;
+                j = i + 1;
+            }
         }
 
-        return even >= 2;
+        return false;
     }
 };
