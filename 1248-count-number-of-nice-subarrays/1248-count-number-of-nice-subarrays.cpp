d44f9348a0e164401810b2ab4cace491d8e2e815
Diksha Deepak <dikshadeepak0195@gmail.com>
2026-07-12 14:50:09 +0530

    Time: 2 ms (91.35%), Space: 71.4 MB (95.17%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,36 @@
+
+
+class Solution {
+public:
+    // function to count subarrays with at most k odd numbers
+    int countAtMost(vector<int>& nums, int k) {
+        // Initialize variables
+        int left = 0, res = 0;
+
+        // Traverse through the array
+        for (int right = 0; right < nums.size(); right++) {
+            // If current number is odd, reduce k
+            if (nums[right] % 2 != 0)
+                k--;
+
+            // Shrink the window until k is valid
+            while (k < 0) {
+                if (nums[left] % 2 != 0)
+                    k++;
+                left++;
+            }
+
+            // Add valid subarrays ending at right
+            res += (right - left + 1);
+        }
+
+        // Return the count of valid subarrays
+        return res;
+    }
+
+    // Main function to get number of subarrays with exactly k odd numbers
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return countAtMost(nums, k) - countAtMost(nums, k - 1);
+    }
+};
+
