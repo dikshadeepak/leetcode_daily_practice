e06fb11639ada84a8ab98eaf82667ec6f68f557e
Diksha Deepak <dikshadeepak0195@gmail.com>
2026-08-10 22:05:29 +0530

    Time: 0 ms (100%), Space: 8.5 MB (39.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+    int solve(int n, unordered_map<int, int>& dp) {
+        if (n == 0 || n == 1)
+            return 1;
+
+        // Already calculated
+        if (dp.find(n) != dp.end())
+            return dp[n];
+
+        // Store answer
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
+        return dp[n];
+    }
+
     int climbStairs(int n) {
-        if (n <= 2)
-            return n;
 
-        int first = 1;
-        int second = 2;
+        unordered_map<int, int> dp;
 
-        for (int i = 3; i <= n; i++) {
-            int curr = first + second;
-            first = second;
-            second = curr;
-        }
-
-        return second;
+        return solve(n, dp);
     }
 };
